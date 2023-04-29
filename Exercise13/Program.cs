da5107e107edd0d4c1028212c5a5cecca9cc7994
Uladzimir1367 <130497737+Uladzimir1367@users.noreply.github.com>
2023-04-29 23:34:37 +0300

    добавлено решение 13 задачи

---
+++ Exercise13/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int ValueN = int.Parse(Console.ReadLine());
+
+FindNumber(ValueN);
+void FindNumber(int N)
+{
+   if ((N > 10000) && (N <= 100000))
+   {
+      int a = N/100;
+      int b = a % 10;
+      Console.WriteLine("Третья цифра числа" +b);
+   }
+   if ((N >= 1000) && (N <= 10000))
+   {
+      int a = N/10;
+      int b = a % 10;
+      Console.WriteLine("Третья цифра числа" +b);
+   }   
+   if ((N > 99) && (N < 1000)) 
+   {
+      int a = N % 10;
+      Console.WriteLine("Третья цифра числа" +a);
+   }
+
+   else if(N < 100)
+   {
+      Console.WriteLine("Третьей цифры нет");
+   } 
+}
+

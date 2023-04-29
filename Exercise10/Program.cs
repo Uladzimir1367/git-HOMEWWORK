404927c057de2763c692b1af90cc3f501c0b3b10
Uladzimir1367 <130497737+Uladzimir1367@users.noreply.github.com>
2023-04-29 23:32:59 +0300

    добавлено решение 10 задачи

---
+++ Exercise10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int rand = new Random().Next(100,1000);
+Console.WriteLine("Рандомное число" + rand);
+
+SekondNumber(rand);
+void SekondNumber(int x)
+{
+int a = x / 10;
+int b = a % 10;
+rand = b;
+}
+Console.WriteLine("Найденное число" + rand);

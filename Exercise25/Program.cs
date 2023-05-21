bb7a176cde0aa463f69d077acb180b5d356dee07
Uladzimir1367 <130497737+Uladzimir1367@users.noreply.github.com>
2023-05-21 21:39:10 +0300

    добавил решение 25 задачи

---
+++ Exercise25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень числа");
+int B = int.Parse(Console.ReadLine()!);
+
+FindNumber(A);
+void FindNumber(int A)
+{
+   int i = 1;
+   int Pr = 1;
+   for (i=1; i <= B; i++)  
+   {
+       
+      Pr = Pr*A;
+   }
+   Console.WriteLine(Pr);
+
+     
+}
+
+

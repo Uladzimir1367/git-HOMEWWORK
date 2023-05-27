dc7c6196140ea915965ee30ca57c062a1e6bd18f
Uladzimir1367 <130497737+Uladzimir1367@users.noreply.github.com>
2023-05-27 21:33:33 +0300

    добавлена 36 задача

---
+++ Exercise36/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0 */
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+   int[] res = new int[size];
+   for (int i = 0; i < size; i++)
+   {
+      res[i] = new Random().Next(minValue, maxValue + 1);
+      Console.Write($"{res[i]} ");
+   }
+   return res;
+}
+void GetSum(int[] arr)
+{
+   int Sum = 0;
+   
+   for (int i = 0; i < arr.Length; i++)
+   {
+      if(arr[i] % 2 != 0)
+      {
+         Sum = Sum + arr[i];
+      }
+
+   }
+   Console.WriteLine();
+   Console.WriteLine($"Сумма нечётных элементов в массиве = {Sum}");
+}
+
+
+int[] array = GetArray(5, 1, 15);
+GetSum(array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
